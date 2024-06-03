2aa8c5eb00ff30f214e4c06db44ec7bfd0f2e894
Nataliia <clarioruberia@gmail.com>
2024-06-03 11:43:49 +0300

    add article serializers.py

---
+++ article/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from article.models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'content', 'publication_date', 'author']
+        read_only_fields = ['author', 'publication_date']
