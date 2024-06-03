306519e09f0aaa8c509304520b51a81ead5fba54
Nataliia <clarioruberia@gmail.com>
2024-06-03 11:36:53 +0300

    add article models.py

--- article/models.py
+++ article/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
