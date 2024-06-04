9e5a3dce427777ef384e0000e15b23df57b38374
Nataliia <clarioruberia@gmail.com>
2024-06-04 10:09:33 +0300

    fix to flake8

--- user/serializers.py
+++ user/serializers.py
@@ -9,7 +9,7 @@ User = get_user_model()
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'avatar', 'is_staff', 'is_active')
+        fields = ("id", "email", "avatar", "is_staff", "is_active")
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -17,14 +17,14 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('name', 'email', 'password', 'avatar')
+        fields = ("name", "email", "password", "avatar")
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            email=validated_data['email'],
-            password=validated_data['password'],
-            name=validated_data['name'],
-            avatar=validated_data.get('avatar')
+            email=validated_data["email"],
+            password=validated_data["password"],
+            name=validated_data["name"],
+            avatar=validated_data.get("avatar"),
         )
         return user
 
@@ -32,13 +32,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('name', 'email', 'avatar')
+        fields = ("name", "email", "avatar")
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.avatar = validated_data.get('avatar', instance.avatar)
-        if 'password' in validated_data:
-            instance.set_password(validated_data['password'])
+        instance.name = validated_data.get("name", instance.name)
+        instance.email = validated_data.get("email", instance.email)
+        instance.avatar = validated_data.get("avatar", instance.avatar)
+        if "password" in validated_data:
+            instance.set_password(validated_data["password"])
         instance.save()
         return instance
